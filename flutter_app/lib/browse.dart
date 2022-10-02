865fc2b43677ce0443bc2cf2b5e3198b9408f6ff
lyc <22719973+l-yc@users.noreply.github.com>
2022-10-02 00:39:53 -0400

    feat(flutter): fancier browse page

--- flutter_app/lib/browse.dart
+++ flutter_app/lib/browse.dart
@@ -1,8 +1,10 @@
 import 'dart:async';
+import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_app/solana.dart';
 import 'package:solana/solana.dart';
+import 'package:collection/collection.dart';
 
 class BrowseWidget extends StatefulWidget {
   const BrowseWidget({super.key});
@@ -37,20 +39,102 @@ class _BrowseWidgetState extends State<BrowseWidget> {
     timer.cancel();
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          ListView(
-            scrollDirection: Axis.vertical,
-            shrinkWrap: true,
-            padding: EdgeInsets.all(20),
-            children: _recent_users.map((e) => Text(e.toBase58())).toList(),
+  Widget makeBackground(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 400,
+      decoration: const ShapeDecoration(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(32),
+            bottomRight: Radius.circular(32),
           ),
-        ],
+        ),
+        gradient: LinearGradient(
+          colors: <Color>[
+            Colors.green,
+            Colors.greenAccent,
+          ],
+        ),
+      ),
+      child: const Padding(
+        padding: EdgeInsets.only(top: 48, left: 24.0),
+        child: Text(
+          'Browse',
+          style: TextStyle(
+            fontFamily: 'Nunito',
+            fontWeight: FontWeight.w800,
+            color: Colors.white,
+            fontSize: 36,
+          ),
+        ),
       ),
     );
+  }
+
+  Widget makeUserCard(BuildContext context, Ed25519HDPublicKey e) {
+    //return Text(e.toBase58());
+    return Container(
+      width: double.infinity,
+      height: 400,
+      decoration: ShapeDecoration(
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(16))),
+          color: Colors.white,
+          shadows: [
+            BoxShadow(
+                blurRadius: 8,
+                color: Colors.black12,
+                offset: Offset.fromDirection(pi / 2, 6),
+                spreadRadius: -2)
+          ]),
+      child: Padding(
+        padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+        child: Text(
+          e.toBase58(),
+          style: TextStyle(
+            fontFamily: 'Nunito',
+            //fontWeight: FontWeight.w800,
+            color: Colors.black,
+            //fontSize: 36,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget makeUsers(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.fromLTRB(20, 120, 20, 20),
+      child: _recent_users.isEmpty
+          ? Container(
+              alignment: Alignment.topCenter,
+              margin: EdgeInsets.all(20),
+              child: CircularProgressIndicator(
+                backgroundColor: Colors.lightGreen,
+                color: Colors.green,
+                strokeWidth: 5,
+              ))
+          : Stack(
+              //scrollDirection: Axis.vertical,
+              //shrinkWrap: true,
+              //padding: EdgeInsets.all(20),
+              children: _recent_users.mapIndexed((index, keypair) {
+                return makeUserCard(context, keypair);
+              }).toList(),
+              //children: _recent_users.mapIndexed((index, keypair) {
+              //  return Positioned(
+              //      top: index * 20, child: makeUserCard(context, keypair));
+              //}).toList(),
+            ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(children: [
+      makeBackground(context),
+      makeUsers(context),
+    ]);
   }
 }
