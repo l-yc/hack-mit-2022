ca1498f00b37d8e7a7b73097852ed0db44aa7f7c
lyc <22719973+l-yc@users.noreply.github.com>
2022-10-01 23:34:19 -0400

    refactor(flutter): clean up base layout

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -5,245 +5,25 @@ import 'package:flutter_app/browse.dart';
 import 'package:flutter_app/chat.dart';
 import 'package:flutter_app/profile.dart';
 
-import 'dart:convert';
-import 'dart:io';
-/*
-import 'package:file_picker/file_picker.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter/services.dart' show rootBundle;
-import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
-import 'package:flutter_chat_ui/flutter_chat_ui.dart';
-import 'package:http/http.dart' as http;
-import 'package:image_picker/image_picker.dart';
-import 'package:intl/date_symbol_data_local.dart';
-import 'package:mime/mime.dart';
-import 'package:open_file/open_file.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:uuid/uuid.dart';
-*/
-
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+// This widget is the root of your application.
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'D(E)ATE',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-      ),
-      home: const MyHomePage(title: 'D(E)ATE'),
-    );
-  }
+  State<MyApp> createState() => _MyAppState();
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MyAppState extends State<MyApp> {
+  static const TITLE = "D(E)ATE";
   int _nav_index = 0;
-  /*List<types.Message> _messages = [];
-  final _user = const types.User(id: '82091008-a484-4a89-ae75-a22bf8d6f3ac');
-
-  @override
-  void initState() {
-    super.initState();
-    _loadMessages();
-  }
-
-  void _loadMessages() async {
-    final response = await rootBundle.loadString('assets/messages.json');
-    final messages = (jsonDecode(response) as List)
-        .map((e) => types.Message.fromJson(e as Map<String, dynamic>))
-        .toList();
-
-    setState(() {
-      _messages = messages;
-    });
-  }
-
-  void _handleMessageTap(BuildContext _, types.Message message) async {
-    if (message is types.FileMessage) {
-      var localPath = message.uri;
-
-      if (message.uri.startsWith('http')) {
-        try {
-          final index =
-              _messages.indexWhere((element) => element.id == message.id);
-          final updatedMessage =
-              (_messages[index] as types.FileMessage).copyWith(
-            isLoading: true,
-          );
-
-          setState(() {
-            _messages[index] = updatedMessage;
-          });
-
-          final client = http.Client();
-          final request = await client.get(Uri.parse(message.uri));
-          final bytes = request.bodyBytes;
-          final documentsDir = (await getApplicationDocumentsDirectory()).path;
-          localPath = '$documentsDir/${message.name}';
-
-          if (!File(localPath).existsSync()) {
-            final file = File(localPath);
-            await file.writeAsBytes(bytes);
-          }
-        } finally {
-          final index =
-              _messages.indexWhere((element) => element.id == message.id);
-          final updatedMessage =
-              (_messages[index] as types.FileMessage).copyWith(
-            isLoading: null,
-          );
-
-          setState(() {
-            _messages[index] = updatedMessage;
-          });
-        }
-      }
-
-      await OpenFile.open(localPath);
-    }
-  }
-
-  void _handleFileSelection() async {
-    final result = await FilePicker.platform.pickFiles(
-      type: FileType.any,
-    );
-
-    if (result != null && result.files.single.path != null) {
-      final message = types.FileMessage(
-        author: _user,
-        createdAt: DateTime.now().millisecondsSinceEpoch,
-        id: const Uuid().v4(),
-        mimeType: lookupMimeType(result.files.single.path!),
-        name: result.files.single.name,
-        size: result.files.single.size,
-        uri: result.files.single.path!,
-      );
-
-      _addMessage(message);
-    }
-  }
-
-  void _addMessage(types.Message message) {
-    setState(() {
-      _messages.insert(0, message);
-    });
-  }
-
-  void _handleAttachmentPressed() {
-    showModalBottomSheet<void>(
-      context: context,
-      builder: (BuildContext context) => SafeArea(
-        child: SizedBox(
-          height: 144,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              TextButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                  _handleImageSelection();
-                },
-                child: const Align(
-                  alignment: AlignmentDirectional.centerStart,
-                  child: Text('Photo'),
-                ),
-              ),
-              TextButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                  _handleFileSelection();
-                },
-                child: const Align(
-                  alignment: AlignmentDirectional.centerStart,
-                  child: Text('File'),
-                ),
-              ),
-              TextButton(
-                onPressed: () => Navigator.pop(context),
-                child: const Align(
-                  alignment: AlignmentDirectional.centerStart,
-                  child: Text('Cancel'),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-
-  void _handleImageSelection() async {
-    final result = await ImagePicker().pickImage(
-      imageQuality: 70,
-      maxWidth: 1440,
-      source: ImageSource.gallery,
-    );
-
-    if (result != null) {
-      final bytes = await result.readAsBytes();
-      final image = await decodeImageFromList(bytes);
-
-      final message = types.ImageMessage(
-        author: _user,
-        createdAt: DateTime.now().millisecondsSinceEpoch,
-        height: image.height.toDouble(),
-        id: const Uuid().v4(),
-        name: result.name,
-        size: bytes.length,
-        uri: result.path,
-        width: image.width.toDouble(),
-      );
-
-      _addMessage(message);
-    }
-  }
-
-  void _handlePreviewDataFetched(
-    types.TextMessage message,
-    types.PreviewData previewData,
-  ) {
-    final index = _messages.indexWhere((element) => element.id == message.id);
-    final updatedMessage = (_messages[index] as types.TextMessage).copyWith(
-      previewData: previewData,
-    );
-
-    setState(() {
-      _messages[index] = updatedMessage;
-    });
-  }
-
-  void _handleSendPressed(types.PartialText message) {
-    final textMessage = types.TextMessage(
-      author: _user,
-      createdAt: DateTime.now().millisecondsSinceEpoch,
-      id: const Uuid().v4(),
-      text: message.text,
-    );
-
-    _addMessage(textMessage);
-  }
-  */
 
   @override
   Widget build(BuildContext context) {
     Widget child = Container();
-
     switch (_nav_index) {
       case 0:
         child = BrowseWidget();
@@ -257,34 +37,40 @@ class _MyHomePageState extends State<MyHomePage> {
         break;
     }
 
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
+    return MaterialApp(
+      title: 'D(E)ATE',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
       ),
-      body: child,
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _nav_index,
-        onTap: (int index) => setState(() => _nav_index = index),
-        type: BottomNavigationBarType.fixed,
-        selectedItemColor: Colors.blue[700],
-        selectedFontSize: 13,
-        unselectedFontSize: 13,
-        iconSize: 30,
-        items: [
-          BottomNavigationBarItem(
-            label: "Browse",
-            icon: Icon(Icons.search),
-          ),
-          BottomNavigationBarItem(
-            label: "Chat",
-            icon: Icon(Icons.chat_bubble_rounded),
-          ),
-          BottomNavigationBarItem(
-            label: "My Account",
-            icon: Icon(Icons.account_circle_outlined),
-          ),
-        ],
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(TITLE),
+        ),
+        body: child,
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _nav_index,
+          onTap: (int index) => setState(() => _nav_index = index),
+          type: BottomNavigationBarType.fixed,
+          selectedItemColor: Colors.blue[700],
+          selectedFontSize: 13,
+          unselectedFontSize: 13,
+          iconSize: 30,
+          items: [
+            BottomNavigationBarItem(
+              label: "Browse",
+              icon: Icon(Icons.search),
+            ),
+            BottomNavigationBarItem(
+              label: "Chat",
+              icon: Icon(Icons.chat_bubble_rounded),
+            ),
+            BottomNavigationBarItem(
+              label: "My Account",
+              icon: Icon(Icons.account_circle_outlined),
+            ),
+          ],
+        ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
